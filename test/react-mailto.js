c5f038b7b739d1bb4ed4561243c74dbd468188f8
Jason Bellamy <j@sonbellamy.com>
2015-09-05 18:29:29 -0400

    update tests to use the shallowOutput module

--- test/react-mailto.js
+++ test/react-mailto.js
@@ -1,26 +1,21 @@
 import { expect } from 'chai';
 import React from 'react/addons';
+import shallowOutput from 'react-shallow-output';
 import Mailto from '../src/react-mailto.js';
-
-const createComponent = (component, props) => {
-  const renderer = React.addons.TestUtils.createRenderer();
-  renderer.render(React.createElement(component, props));
-  return renderer.getRenderOutput();
-};
 
 describe('Mailto component', () => {
   it('should hide the email address if obfuscate is true', () => {
-    const component = createComponent(Mailto, {email: 'j@sonbellamy.com', obfuscate: true});
+    const component = shallowOutput(Mailto, {email: 'j@sonbellamy.com', obfuscate: true});
     expect(component.props.href).to.equal('mailto:obfuscated');
   });
 
   it('should not hide the email address if obsfuscate is false', () => {
-    const component = createComponent(Mailto, {email: 'j@sonbellamy.com', obfuscate: false});
+    const component = shallowOutput(Mailto, {email: 'j@sonbellamy.com', obfuscate: false});
     expect(component.props.href).to.equal('mailto:j@sonbellamy.com');
   });
 
   it('should not hide the email address if obsfuscate is not set', () => {
-    const component = createComponent(Mailto, {email: 'j@sonbellamy.com'});
+    const component = shallowOutput(Mailto, {email: 'j@sonbellamy.com'});
     expect(component.props.href).to.equal('mailto:j@sonbellamy.com');
   });
 });
